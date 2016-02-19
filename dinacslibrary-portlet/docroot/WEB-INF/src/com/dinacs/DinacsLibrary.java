c79d67d73cea28c4ae9a496777517738ec0be103
datta <datta@datta-PC>
2016-02-19 12:19:12 -0800

    commiting list.jsp

--- dinacslibrary-portlet/docroot/WEB-INF/src/com/dinacs/DinacsLibrary.java
+++ dinacslibrary-portlet/docroot/WEB-INF/src/com/dinacs/DinacsLibrary.java
@@ -1,18 +1,20 @@
 package com.dinacs;
 
-import java.io.IOException;
+import java.io.IOException; 
 
 import javax.portlet.ActionRequest;
 import javax.portlet.ActionResponse;
 import javax.portlet.PortletException;
 
+import com.liferay.counter.service.CounterLocalServiceUtil;
+import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.util.bridges.mvc.MVCPortlet;
 
-public class DinacsLibrary extends MVCPortlet{
+public class DinacsLibrary<LMSBook> extends MVCPortlet{
 	public void updateBook(ActionRequest actionRequest,
 			ActionResponse actionResponse)
-			throws IOException, PortletException
+			throws IOException, PortletException, SystemException
 			{
 				String bookTitle = ParamUtil.getString(actionRequest, "bookTitle");
 				String author = ParamUtil.getString(actionRequest, "author");
@@ -24,8 +26,31 @@ public class DinacsLibrary extends MVCPortlet{
 				String termsAndCondition = ParamUtil.getString(actionRequest, "termsAndCondition");
 				
 				
+				
 				System.out.println("Your inputs ==> " + bookTitle + ", " + author + ","  + bookId + ", " + bookType+ "," + termsAndCondition);
+			
+				
+				Book Book1 = new Book();
+				long bookId1 = 0l;
+				 try {
+					 bookId1 = CounterLocalServiceUtil.increment();
+				 } 
+				catch (SystemException e)
+				 {
+				 e.printStackTrace();
+				 } 
+				
+
+				Book1.setBookId((int) bookId1); 
+				Book1.setBookTitle(bookTitle);
+				Book1.setAuthor(author);
+				
+
+				
+				Book1 = Book1LocalServiceUtil.addBook1(Book1);
+				
 				
 			}
 
+	
 }
